13dfa29013b023b2ccd09696bdac77c5ae509f6c
AV1mando <alanisarmando93@gmail.com>
2025-04-13 00:28:23 -0600

    Add files via upload

---
+++ proyecto/hacer_pedido/pedido/main.js
@@ -0,0 +1,22 @@
+// add hovered class to selected List item
+let list = document.querySelectorALL(".navigation li");
+
+function activelink() {
+    list.forEach((item) => {    
+    item.classList.remove("hovered");
+});
+this.classList.add("hovered");
+}
+
+list.forEach((item) => item.addEventListener("mouseover", activelink));
+
+// Menu Toggle
+
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+    navigation.classList.toggle("active");
+    main.classList.toggle("active");
+};
